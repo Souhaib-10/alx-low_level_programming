41a5e2f5c03bc67e2e4796bbb5382b83bc6ba5c6
Souhaib-10 <souhaibb1998@gmail.com>
2023-09-04 11:25:00 -0700

    program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(int argc, char *argv[])
+{
+	printf("Number of arguments: %d\n", argc - 1);
+	return 0;
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+File"1-args.c" program that prints its name, followed by a new line.
